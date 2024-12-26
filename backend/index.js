eb921a17223024f6aaad75a79ed408d65ae95492
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2024-12-26 19:57:38 +0530

    login-api

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,39 @@ app.post("/create-account", async (req, res) => {
     }
 });
 
-// Start Server
+//Login API
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and Password are required" })
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.status(400).json({ message: "User not found" })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: "Invalid Credentials" })
+    }
+
+    const accessToken = jwt.sign(
+        { userId: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72hr",
+        }
+    )
+
+    return res.json({
+        error: false, message: "Login Successful",
+        user: { fullName: user.fullName, email: user.email },
+        accessToken,
+    })
+})
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
